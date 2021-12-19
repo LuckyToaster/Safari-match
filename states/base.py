7e80e0185172b08e98f27e8a37b5225639797233
Senén <senen.marques@live.u-tad.com>
2021-12-19 21:56:40 +0100

    'added some bs'

--- states/base.py
+++ states/base.py
@@ -8,11 +8,17 @@ class BaseState(object):
         self.next_state = None
         self.screen_rect = pg.display.get_surface().get_rect()
         self.persist = {}
-        self.font = pg.font.Font(None, 24)
+        self.font = pg.font.Font("assets/KarmaFuture.ttf", 40)
         # things I've added
         self.backgrounds = os.listdir("./assets/backgrounds")
         self.screen_w = pg.display.Info().current_w
         self.screen_h = pg.display.Info().current_h
+        self.colors = {
+            "CYAN": (172,238,243),
+            "CORAL": (255,112,119),
+            "ROSE": (255,233,228),
+            "ORANGE": (255,176,103)
+        }
 
     def startup(self, persistent):
         self.persist = persistent
--- states/startscreen.py
+++ states/startscreen.py
@@ -4,12 +4,14 @@ from .base import BaseState
 class StartScreen(BaseState):
     def __init__(self):
         super(StartScreen, self).__init__()
-        self.title = self.font.render("Safari Match!", True, pg.Color("blue"))
+        self.title_string = "Safari Match!"
+        self.font = pg.font.Font("assets/KarmaFuture.ttf", 80)
+        self.rand_color = list(self.colors.values())[random.randrange(len(self.colors))] # get a random color
+        self.title = self.font.render("Safari Match!", True, self.rand_color)
         self.title_rect = self.title.get_rect(center=self.screen_rect.center)
         self.next_state = "MAIN_MENU"
         self.time_active = 0
-        # get a random bg image for every startscreen
-        self.ran_bg_index = random.randrange(len(self.backgrounds))
+        self.ran_bg_index = random.randrange(len(self.backgrounds)) # get a random bg image for every startscreen
 
     def update(self, dt):
         self.time_active += dt
@@ -19,6 +21,8 @@ class StartScreen(BaseState):
     def draw(self, surface):
         bg_img = pg.image.load("assets/backgrounds/" + self.backgrounds[self.ran_bg_index]) # get a random background
         bg_img = pg.transform.smoothscale(bg_img,(self.screen_w,self.screen_h)) # scale it
-        surface.blit(bg_img, (0,0)) # draw it
-        surface.blit(self.title, self.title_rect) # draw the game title
+        surface.blit(bg_img, (0,0)) # draw background
+        surface.blit(self.title, self.title_rect)
+
+
 
